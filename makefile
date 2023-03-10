929706c282bfefd2d5a5a5b203d57c9768c65859
zearkiatos <caprilespe@outlook.com>
2023-03-09 22:59:38 -0300

    Generation the random data

--- makefile
+++ makefile
@@ -5,3 +5,4 @@ run-tests:
 	 ENVIRONMENT=test python -m unittest discover -s tests -p 'test*.py' -v
 	 coverage run -m unittest discover
 	 coverage report -m
+	 coverage html
--- tests/model/test_person.py
+++ tests/model/test_person.py
@@ -1,11 +1,19 @@
 import unittest
 import datetime
+from faker import Faker
 from src.model.Person import Person
 from src.Community.base import Session, engine, Base
 
 
 class PersonTestCase(unittest.TestCase):
     def setUp(self):
+        self.data_factory = Faker()
+        self.data = []
+        self.people = []
+        for i in range(0, 10):
+            self.data.append(
+                (self.data_factory.name(), self.data_factory.random_number()))
+            self.people.append(Person(name=self.data[-1][0], year=self.data[-1][-1]))
         self.session = Session()
         self.person1 = Person(name="Alejandra", year=25)
         self.person2 = Person(name="Diego", year=22)
@@ -72,3 +80,59 @@ class PersonTestCase(unittest.TestCase):
 
         self.assertEqual(person.get_name(), 'Diego')
         self.assertEqual(person.get_year(), 22)
+
+    def test_constructor_random(self):
+        for person, dat in zip(self.people, self.data):
+            self.assertEqual(person.get_name(), dat[0])
+            self.assertEqual(person.get_year(), dat[-1])
+
+    def test_birth_year_random(self):
+        for person, dat in zip(self.people, self.data):
+            self.assertEqual(person.calculate_birth_day(
+                True), datetime.datetime.now().year - dat[-1])
+
+    def test_assign_random(self):
+        original_data = (self.data_factory.name(), self.data_factory.random_number())
+        test_person = Person(name=original_data[0], year=original_data[-1])
+        new_data = (self.data_factory.name(), self.data_factory.random_number())
+        while new_data[0] == original_data[0] or new_data[-1] == original_data[-1]:
+            new_data = (self.data_factory.name(), self.data_factory.random_number())
+        test_person.set_name(new_data[0])
+        test_person.set_year(new_data[-1])
+
+        self.assertFalse(test_person.get_name() == original_data[0])
+        self.assertFalse(test_person.get_year() == original_data[-1])
+        self.assertTrue(test_person.get_name() == new_data[0])
+        self.assertTrue(test_person.get_year() == new_data[-1])
+
+    def test_objects_equals_random(self):
+        new_person = self.people[-1]
+
+        self.assertIsNot(new_person, self.people[0])
+        self.assertIs(new_person, self.people[-1])
+
+    def test_element_in_group_random(self):
+        self.assertIsInstance(self.people[0], Person)
+        self.assertNotIsInstance(self.people, Person)
+
+    def test_store_random(self):
+        self.people[0].store()
+
+        self.session = Session()
+        person = self.session.query(Person).filter(
+            Person.name == self.data[0][0] and Person.year == self.data[0][1]).first()
+
+        self.assertEqual(person.get_name(), self.data[0][0])
+        self.assertEqual(person.get_year(), self.data[0][-1])
+
+    def test_find_random(self):
+        self.session = Session()
+        self.session.add(self.people[0])
+        self.session.commit()
+        self.session.close()
+
+        person = Person("", 0)
+        person.find(self.data[0][0], self.data[0][-1])
+
+        self.assertEqual(person.get_name(), self.data[0][0])
+        self.assertEqual(person.get_year(), self.data[0][-1])
